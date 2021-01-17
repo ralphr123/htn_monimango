57310797589441ae01261df86334377938bcaf2a
Steven.J.Li <14027496@student.uts.edu.au>
2021-01-17 11:32:48 +1100

    added carousel

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,6 +4,8 @@ import './LandingPage.css'
 import logo from '../../images/MoniMango_logo.svg'
 import { Link } from 'react-tiger-transition';
 import screenshot from '../../images/screenshot.png';
+import Slide from 'react-reveal/Slide';
+import Carousel from '../../components/Carousel/Carousel';
 
 export default function LandingPage() {
     return (
@@ -20,16 +22,35 @@ export default function LandingPage() {
             
             </div>
             <div className="about-container">
-                <div className="logo">
-                    <img src={logo} alt="MoniMango" className="logo-img"/>
-                    <h1 className="logo-name">MoniMango</h1>
-                </div>
-                <div className="description">
-                Social Media has a large influence on the stock market.
+                <Carousel defaultWait={3000}>
 
-            Our app aims to look at what stocks are currently being favoured or dismissed by people on social media using a sentimental analysis model.
-                </div>
-                <img src={screenshot} alt="Screenshot" class="screenshot"/>
+                <Slide right>
+                    <div className="content">
+                        <div className="logo">
+                            <img src={logo} alt="MoniMango" className="logo-img"/>
+                            <h1 className="logo-name">MoniMango</h1>
+                        </div>
+                        <div className="description">
+                        Social Media has a large influence on the stock market.
+                        <br/>
+                        MoniMango aims to look at what stocks are currently being favoured or dismissed by people on social media using a sentimental analysis model.
+                        <br/>   
+                        <br/>
+                        Not sure what to do with your current shares? Looking to dive into the stock market? We'll help you know what others are thinking.
+                        </div>
+                    </div>
+                </Slide>
+                <Slide right>
+                    <div className="content two">
+                        <img src={screenshot} alt="Screenshot" class="screenshot"/>
+                        <div className="description 2">
+                            Find out the top stocks that are being mentioned on famous social media platsforms like Reddit. Receive recommendations on whether to buy hold or sell.
+                        </div>
+                    </div>
+                </Slide>
+                </Carousel>
+
+                
             </div>
         </div>
         
