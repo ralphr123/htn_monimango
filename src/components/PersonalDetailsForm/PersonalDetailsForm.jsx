5c53b02d8f798cce7cc310ea8ad4914b0f6e6700
Ralph Rouhana <rrouhana@sipstack.com>
2021-01-17 09:44:25 -0500

    edit

--- src/components/PersonalDetailsForm/PersonalDetailsForm.jsx
+++ src/components/PersonalDetailsForm/PersonalDetailsForm.jsx
@@ -13,7 +13,7 @@ export default function PersonalDetailsForm(){
             method: 'post',
             url: 'https://wicked-phantom-05767.herokuapp.com/https://cryptic-woodland-77043.herokuapp.com/newsletter',
             data: {
-                number: "15877183475"
+                number: number
             }
         }).then((response) => {
             window.localStorage.setItem("mm_user", JSON.stringify({ number, name, email }));
@@ -43,7 +43,7 @@ export default function PersonalDetailsForm(){
         <form id="form" className="form">
             <input type="text" name="name" value={form.name} required onChange={(e) => handleInputChange(e)} placeholder= "Full Name"/>
             {/* <input type="email" name="email" value={form.email} required onChange={(e) => handleInputChange(e)} placeholder="Email Address"/> */}
-            <input type="tel" name="number" value={form.number} required onChange={(e) => handleInputChange(e)} placeholder="Phone Number (ex. 1234567890)"/>
+            <input type="text" name="number" value={form.number} required onChange={(e) => handleInputChange(e)} placeholder="Phone Number (ex. 1234567890)"/>
             <span className="form-consent">By submitting this form, you consent to recieving bi-weekly updates on top stocks by SMS for the next 30 days.</span>
             <button className="continue" type="submit" onClick={(e) => postNewsLetter(e, form.number, form.name, form.email)}>Continue</button>
         </form>  
